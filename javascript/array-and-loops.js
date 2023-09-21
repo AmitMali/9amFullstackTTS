bf6e03b05eb37b52ef25510ec3ad86d2554fb4fb
AmitMali <amit23289@gmail.com>
2023-09-21 09:20:17 +0530

    js functions

---
+++ javascript/array-and-loops.js
@@ -0,0 +1,21 @@
+var data = ["amit mali", 23, true, ["python", "js", "php", "html", "css"]];
+
+// for (var i = 0; i < data.length; i++) {
+//   console.log(data[i]);
+// }
+
+// var total = 0;
+// for (var i = 1; i <= 10; i++) {
+//   total = total + i;
+// }
+// console.log(total);
+// var i = 1;
+// while (i <= 10) {
+//   total = total + i;
+//   i++;
+// }
+// console.log(total);
+
+// data.forEach((x) => {
+//   console.log(x);
+// });
---
+++ javascript/functions.js
@@ -0,0 +1,17 @@
+function hello(name = "user") {
+  console.log("hello " + name);
+}
+
+hello("amit");
+hello("Dipali");
+hello("Rohit");
+hello("Rupali");
+hello("Harshwardhan");
+hello("Jay");
+hello();
+
+function add(no1, no2) {
+  return no1 + no2;
+}
+
+console.log(add(10, 20) + 40);
