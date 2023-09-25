cbdf8f5b54914dde8b40499d303745e2c395e515
AmitMali <amit23289@gmail.com>
2023-09-25 10:11:57 +0530

    operator

---
+++ javascript/foo.js
@@ -0,0 +1,6 @@
+// var fname = true;
+// console.log(typeof fname);
+// console.log(2 ** 3);
+// console.log(5 % 2);
+
+// console.log(10 === "10");
--- javascript/functions.js
+++ javascript/functions.js
@@ -2,16 +2,32 @@ function hello(name = "user") {
   console.log("hello " + name);
 }
 
-hello("amit");
-hello("Dipali");
-hello("Rohit");
-hello("Rupali");
-hello("Harshwardhan");
-hello("Jay");
-hello();
+// hello("amit");
+// hello("Dipali");
+// hello("Rohit");
+// hello("Rupali");
+// hello("Harshwardhan");
+// hello("Jay");
+// hello();
 
 function add(no1, no2) {
   return no1 + no2;
 }
 
-console.log(add(10, 20) + 40);
+// console.log(add(10, 20) + 40);
+
+function addAll(...nums) {
+  var total = 0;
+  for (var i = 0; i < nums.length; i++) {
+    total = total + nums[i];
+  }
+  return total;
+}
+
+console.log(addAll(23, 445, 67, 88, 96, 45, 67));
+
+var [no1, no2] = [23, 45];
+// console.log(no1, no2);
+
+var [no3, no4, ...rem] = [23, 56, 78, 98, 99, 75, 34];
+console.log(no3, no4, rem);
