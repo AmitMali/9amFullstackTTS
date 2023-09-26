7c6696458621783308f00877d92e1dff57cd5dbb
AmitMali <amit23289@gmail.com>
2023-09-26 09:34:15 +0530

    var let const hoisting arrow function

---
+++ javascript/arrow_function.js
@@ -0,0 +1,18 @@
+function add(no1, no2) {
+  return no1 + no2;
+}
+console.log(add(10, 20));
+
+const add1 = (no1, no2) => {
+  return no1 + no2;
+};
+
+console.log(add1(30, 50));
+
+const foo = (name) => console.log(name);
+
+foo("amit");
+
+const nums = [23, 45, 56, 898, 656, 89, 45];
+
+nums.forEach((x) => console.log(x));
---
+++ javascript/var_let_const_hoisting.js
@@ -0,0 +1,9 @@
+// const x = "foo";
+// console.log(x);
+// // x = "hello";
+// console.log(x);
+
+// {
+//   var foo = "hello";
+// }
+// console.log(foo);
