0c9653f47805e86b07f92774f0973556af9bf016
AmitMali <amit23289@gmail.com>
2023-10-03 09:52:16 +0530

    promise with example

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,24 +1,62 @@
-function getUsers() {
-  let users = [];
-  //   users = [
-  //     { username: "john", email: "john@test.com" },
-  //     { username: "jane", email: "jane@test.com" },
-  //   ];
-  setTimeout(() => {
-    users = [
-      { username: "john", email: "john@test.com" },
-      { username: "jane", email: "jane@test.com" },
-    ];
-  }, 189);
+// function getUsers() {
+//   let users = [];
+//   //   users = [
+//   //     { username: "john", email: "john@test.com" },
+//   //     { username: "jane", email: "jane@test.com" },
+//   //   ];
+//   setTimeout(() => {
+//     users = [
+//       { username: "john", email: "john@test.com" },
+//       { username: "jane", email: "jane@test.com" },
+//     ];
+//   }, 189);
 
-  return users;
-}
+//   return users;
+// }
 
-function findUser(username) {
-  const users = getUsers();
-  console.log(users);
-  const user = users.find((user) => user.username === username);
-  return user;
-}
+// function findUser(username) {
+//   const users = getUsers();
+//   console.log(users);
+//   const user = users.find((user) => user.username === username);
+//   return user;
+// }
 
-console.log(findUser("john"));
+// console.log(findUser("john"));
+
+const myPromise = new Promise((resolve, reject) => {
+  if (10 % 2 == 0) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+myPromise
+  .then(() => {
+    console.log("its even number");
+  })
+  .catch(() => {
+    console.log("its not even number");
+  });
+
+const getUsers = new Promise((resolve, reject) => {
+  try {
+    let users = [];
+    setTimeout(() => {
+      users = [
+        { username: "john", email: "john@test.com" },
+        { username: "jane", email: "jane@test.com" },
+      ];
+      resolve(users);
+    }, 2000);
+  } catch (e) {
+    reject("not able to load users");
+  }
+});
+
+getUsers
+  .then((res) => {
+    const user = res.find((user) => user.username === "john");
+    return user;
+  })
+  .catch((err) => console.log(err));
