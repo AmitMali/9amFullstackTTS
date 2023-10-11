b4d661a703159ab11c6ef6739c6572dc5c4d73a8
AmitMali <amit23289@gmail.com>
2023-10-11 09:18:39 +0530

    next setup and layoute component creation initial setup

---
+++ learnNextJs/learn_react/app/components/leftSidebar.jsx
@@ -0,0 +1,28 @@
+export default function LeftSidebar() {
+  return (
+    <nav>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            Active
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">
+            Disabled
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ learnNextJs/learn_react/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Hello ,Welcome to react</h1>
+      <p>lorem ipsum</p>;
+    </>
+  );
+}
