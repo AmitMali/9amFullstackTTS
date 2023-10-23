e93245aa51f8c0aca987f22f15a754d518ebd334
AmitMali <amit23289@gmail.com>
2023-10-23 09:37:57 +0530

    form handling

--- learnNextJs/learn_react/app/signup/page.jsx
+++ learnNextJs/learn_react/app/signup/page.jsx
@@ -3,12 +3,7 @@ import SignupForm from "../components/signupForm/signupForm";
 const Signup = () => {
   return (
     <>
-      <div className="main__container">
-        <p className="signup__header">Signup</p>
-        <div className="signup__col">
-          <SignupForm />
-        </div>
-      </div>
+      <SignupForm />
     </>
   );
 };
